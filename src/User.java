5787a852b12860ee462431f5e17620d3265961de
ahmednader10 <ahmednader839@gmail.com>
2017-03-29 13:46:48 +0200

    generated digital signature

--- src/User.java
+++ src/User.java
@@ -1,3 +1,13 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Signature;
 
 public class User {
 	long q;
@@ -15,6 +25,47 @@ public class User {
 	public void generateY(){
 		this.Y = (a^this.X)%q;
 	}
+	public void generateDigitalSignature(File PublicKey) {
+		try {
+
+			KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
+			
+			SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+			keyGen.initialize(1024, random);
+			
+			KeyPair pair = keyGen.generateKeyPair();
+			PrivateKey priv = pair.getPrivate();
+			PublicKey pub = pair.getPublic();
+			
+			Signature dsa = Signature.getInstance("SHA1withDSA", "SUN");
+			dsa.initSign(priv);
+			
+			FileInputStream fis = new FileInputStream(PublicKey);
+			BufferedInputStream bufin = new BufferedInputStream(fis);
+			byte[] buffer = new byte[1024];
+			int len;
+			while ((len = bufin.read(buffer)) >= 0) {
+			    dsa.update(buffer, 0, len);
+			};
+			bufin.close();
+			byte[] realSig = dsa.sign();
+			
+			/* save the signature in a file */
+			FileOutputStream sigfos = new FileOutputStream("sig");
+			sigfos.write(realSig);
+			sigfos.close();
+			
+			/* save the public key in a file */
+			byte[] key = pub.getEncoded();
+			FileOutputStream keyfos = new FileOutputStream("suepk");
+			keyfos.write(key);
+			keyfos.close();
+
+	        } catch (Exception e) {
+	            System.err.println("Caught exception " + e.toString());
+	        }
+	}
+	
 	public long getQ() {
 		return q;
 	}
