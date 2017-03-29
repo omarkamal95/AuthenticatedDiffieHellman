4f00c16ae73515e1ed0c6e096fb7cfa2cd00fa02
omarkamal95 <omarkamal95@gmail.com>
2017-03-29 13:42:13 +0200

    added pubkey verification

--- src/User.java
+++ src/User.java
@@ -1,10 +1,24 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
 
 public class User {
 	long q;
 	long a;
 	long X;
 	long Y;
-	
+
 	public User(long Qinit, long Ainit){
 		this.q = Qinit;
 		this.a = Ainit;
@@ -15,6 +29,46 @@ public class User {
 	public void generateY(){
 		this.Y = (a^this.X)%q;
 	}
+
+	public boolean verifySignature(File yaSender, File sigYaSender, File pubSender) throws IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException, InvalidKeyException, SignatureException{
+		FileInputStream keyfis = new FileInputStream(yaSender);
+		byte[] encKey = new byte[keyfis.available()];  
+		keyfis.read(encKey);
+
+		keyfis.close();
+		X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encKey);
+		KeyFactory keyFactory = KeyFactory.getInstance("DSA", "SUN");
+		PublicKey pubKey = keyFactory.generatePublic(pubKeySpec);
+		
+		FileInputStream sigfis = new FileInputStream(sigYaSender);
+		byte[] sigToVerify = new byte[sigfis.available()]; 
+		sigfis.read(sigToVerify);
+		sigfis.close();
+		
+		Signature sig = Signature.getInstance("SHA1withDSA", "SUN");
+		sig.initVerify(pubKey);
+		
+		FileInputStream datafis = new FileInputStream(pubSender);
+		BufferedInputStream bufin = new BufferedInputStream(datafis);
+
+		byte[] buffer = new byte[1024];
+		int len;
+		while (bufin.available() != 0) {
+		    len = bufin.read(buffer);
+		    sig.update(buffer, 0, len);
+		};
+
+		bufin.close();
+		
+		boolean verifies = sig.verify(sigToVerify);
+		System.out.println("signature verifies: " + verifies);
+		
+		return verifies;
+
+
+	}
+
+
 	public long getQ() {
 		return q;
 	}
@@ -39,6 +93,6 @@ public class User {
 	public void setY(long y) {
 		Y = y;
 	}
-	
-	
+
+
 }
